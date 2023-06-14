365e7298aca9079ab2d19d5a7ea4df11ccf48b33
Matias Verdier <matias.verdier@wyeworks.com>
2023-06-14 13:43:47 -0300

    Use Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import splitbee from '@splitbee/web';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    splitbee.init();
-  }, []);
-
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
