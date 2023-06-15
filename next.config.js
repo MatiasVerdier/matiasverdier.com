d9dd29f6fd201186193e08f5b595fd96a37efa01
Matias Verdier <matias.verdier@wyeworks.com>
2023-06-15 14:20:43 -0300

    config uploads folder and tina images domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   images: {
-    domains: ['images.prismic.io', 'dev-to-uploads.s3.amazonaws.com'],
+    domains: [
+      'images.prismic.io',
+      'dev-to-uploads.s3.amazonaws.com',
+      'assets.tina.io',
+    ],
   },
   async rewrites() {
     return [
